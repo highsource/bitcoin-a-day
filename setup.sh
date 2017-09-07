003068d3e8c17162237b8878cb744d678c96ce79
Alexey Valikov <aleksei.valikov@gmail.com>
2017-09-08 00:26:24 +0200

    Added generation of a KMS key.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+owner_id=$(aws ec2 describe-security-groups --group-names 'Default' --query 'SecurityGroups[0].OwnerId' --output text)
+
 # Encrypt bitstamp-properties.json
 echo Generating password for the bitstamp-properties.json file.
 bitstamp_properties_password=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1)
@@ -22,7 +24,7 @@ aws s3 cp bitstamp-properties.json.encrypted "${s3_bucket_uri}"
 
 # Create IAM policy to access bitstamp-properties.json.encrypted
 iam_policy_name=bitcoin-a-day-"${random_id}"-bitstamp-properties-access
-sed -e s/@bucket_name@/"${bucket_name}"/g bitstamp-properties-access-policy-template.json > bitstamp-properties-access-policy.json
+sed -e s!@bucket_name@!"${bucket_name}"!g bitstamp-properties-access-policy-template.json > bitstamp-properties-access-policy.json
 echo Creating IAM policy "${iam_policy_name}" to access bitstamp-properties.json.encrypted file
 aws iam create-policy --policy-name "${iam_policy_name}" --policy-document file://bitstamp-properties-access-policy.json --output text
 echo IAM policy name: "${iam_policy_name}" >> output.txt
@@ -33,11 +35,24 @@ echo IAM policy ARN: "${iam_policy_arn}" >> output.txt
 # Create IAM role for the lambda function
 iam_role_name=bitcoin-a-day-"${random_id}"-lambda
 echo Creating IAM role "${iam_role_name}" for the lambda function.
-aws iam create-role --role-name "${iam_role_name}" --assume-role-policy-document file://bitcoin-a-day-lambda-role-policy-document.json
-echo IAM role for the lambda function: ${iam_role_name} >> output.txt
+aws iam create-role --role-name "${iam_role_name}" --assume-role-policy-document file://bitcoin-a-day-lambda-role-policy-document.json --query "Role.Arn" --output text
+iam_role_arn_query='Roles[?RoleName==`'"${iam_role_name}"'`].Arn'
+iam_role_arn=$(aws iam list-roles --query "${iam_role_arn_query}" --output text)
+echo IAM role name for the lambda function: ${iam_role_name} >> output.txt
+echo IAM role ARN for the lambda function: ${iam_role_arn} >> output.txt
 
 echo Attaching IAM policy "${iam_policy_arn}" to lambda function role "${iam_role_name}"
 aws iam attach-role-policy --role-name "${iam_role_name}" --policy-arn "${iam_policy_arn}"
 
 echo Attaching IAM policy arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole to lambda function role "${iam_role_name}"
 aws iam attach-role-policy --role-name "${iam_role_name}" --policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole
+
+echo Creating encryption key policy.
+sed -e "s!@owner_id@!${owner_id}!g; s!@iam_role_arn@!${iam_role_arn}!g" encryption-key-policy-template.json > encryption-key-policy.json
+echo Creating encryption key.
+encryption_key_id=$(aws kms create-key --policy fileb://encryption-key-policy.json --description "Bitcoin A Day" --query KeyMetadata.KeyId --output text)
+echo Encryption key id: ${encryption_key_id} >> output.txt
+encryption_key_alias=alias/bitcoin-a-day-"${random_id}"-encryption-key
+echo Creating encryption key alias "${encryption_key_alias}".
+aws kms create-alias --alias-name "${encryption_key_alias}" --target-key-id "${encryption_key_id}"
+echo Encryption key alias: ${encryption_key_alias} >> output.txt
