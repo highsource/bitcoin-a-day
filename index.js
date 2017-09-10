3124e253ec524f294a738c05900d2ef45404adb1
Alexey Valikov <aleksei.valikov@gmail.com>
2017-09-10 12:17:18 +0200

    Reworked secret properties protection using KMS. Not using S3 to store secret data anymore. Implemented lambda function creation and scheduling.

---
+++ index.js
@@ -0,0 +1,69 @@
+const AWS = require('aws-sdk');
+
+if (!AWS.config.region) {
+	AWS.config.update({region: 'eu-central-1'});
+}
+
+//const ENCRYPTED_BITSTAMP_CUSTOMER_ID = 'AQICAHg3EDivGcQ+weJzvX4THVnJy/nhoHmMQLPamSDzbtVh5wFlRoiFo/Fc11O+ku7sqKK8AAAAZjBkBgkqhkiG9w0BBwagVzBVAgEAMFAGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMFsa9fkDbOpWM1OHVAgEQgCNLzscYCk4dZD9C0+f38RKOEE3zZb7Lev5fZ1M4Skwps4SEVg==';
+//const ENCRYPTED_BITSTAMP_KEY = 'AQICAHg3EDivGcQ+weJzvX4THVnJy/nhoHmMQLPamSDzbtVh5wEY2ZC267Glz7B9saK3Y6smAAAAfjB8BgkqhkiG9w0BBwagbzBtAgEAMGgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQM4BW/1Vf/9Woc+wsRAgEQgDtXo9xyIO1aCOKtNf4DAyviJwTWGoii/0sANeEYkLSxdPQtrkIOTASPfoprPLVTTm0FMV10JTveUR5ozg==';
+//const ENCRYPTED_BITSTAMP_SECRET = 'AQICAHg3EDivGcQ+weJzvX4THVnJy/nhoHmMQLPamSDzbtVh5wGFAoYCI/WyZTMgzb7nTzRHAAAAfjB8BgkqhkiG9w0BBwagbzBtAgEAMGgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMBTvkgfY3L3KU37vWAgEQgDtOAUzcIbVMVoGa5Y6zvQTqBfcCFDzWIfyScUg3ASON0Tgd5KJ2VZ07E+YrUss3BsRcLKBliuABfMQmMw==';
+const ENCRYPTED_BITSTAMP_CUSTOMER_ID = process.env['BITSTAMP_CUSTOMER_ID'];
+const ENCRYPTED_BITSTAMP_KEY = process.env['BITSTAMP_KEY'];
+const ENCRYPTED_BITSTAMP_SECRET = process.env['BITSTAMP_SECRET'];
+//const VALUE = 5;
+const VALUE = Number(process.env['VALUE']);
+
+let BITSTAMP_PROPERTIES;
+
+const MINIMUM_VALUE = 5;
+
+if (isNaN(VALUE) || VALUE < MINIMUM_VALUE) {
+	throw new Error("Invalid value [" + value + "], must be a number not less than [" + MINIMUM_VALUE + "].");
+}
+
+function processEvent(event, context, callback) {
+    // TODO handle the event here
+}
+
+const kms = new AWS.KMS();
+
+const decrypt = function(encrypted) {
+	return new Promise(function(resolve, reject) {
+		kms
+			.decrypt({ CiphertextBlob: new Buffer(encrypted, 'base64') })
+			.promise()
+			.then(data => {console.log('Decrypted:', data); resolve(data.Plaintext.toString('ascii'));})
+			.catch(reject)
+	});
+};
+
+const decryptBitstampProperties = new Promise(function(resolve, reject){
+	Promise
+	.all([
+		decrypt(ENCRYPTED_BITSTAMP_CUSTOMER_ID),
+		decrypt(ENCRYPTED_BITSTAMP_KEY),
+		decrypt(ENCRYPTED_BITSTAMP_SECRET)])
+	.then(values => {
+		BITSTAMP_PROPERTIES = { customerId: values[0], key: values[1], secret: values[2] };
+		resolve(BITSTAMP_PROPERTIES);
+	})
+	.catch(reject);
+});
+
+const doWithBitstampProperties = new Promise(function(resolve, reject){
+	if (BITSTAMP_PROPERTIES) {
+		resolve(BITSTAMP_PROPERTIES);
+	} else {
+		decryptBitstampProperties.then(resolve).catch(reject);
+	}
+});
+
+const doit = function(customerId, key, secret, value) {
+	let message = "Buying " + value + " worth of BTC using customerId " + customerId + ".";
+	console.log(message);
+	return message;
+};
+
+exports.handler = (event, context, callback) => {
+	doWithBitstampProperties.then(bitstampProperties => callback(null, doit(bitstampProperties.customerId, bitstampProperties.key, bitstampProperties.secret, VALUE)));
+};
--- setup.sh
+++ setup.sh
@@ -1,39 +1,19 @@
 #!/bin/bash
-owner_id=$(aws ec2 describe-security-groups --group-names 'Default' --query 'SecurityGroups[0].OwnerId' --output text)
-
-# Encrypt bitstamp-properties.json
-echo Generating password for the bitstamp-properties.json file.
-bitstamp_properties_password=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1)
-echo Encrypting bitstamp-properties.json as bitstamp-properties.json.encrypted.
-openssl enc -e -a -nosalt -pass pass:"${bitstamp_properties_password}" -in bitstamp-properties.json -out bitstamp-properties.json.encrypted -aes-256-cbc
-echo Password to decrypt bitstamp-properties.json.encrypted file: ${bitstamp_properties_password} >> output.txt
-
+bitstamp_customer_id=$1
+bitstamp_key=$2
+bitstamp_secret=$3
+value=$4
 random_id=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1)
+bitcoin_a_day=bitcoin-a-day-"${random_id}"
 
-# Create S3 bucket
-bucket_name=bitcoin-a-day-$random_id
-s3_bucket_uri='s3://'"${bucket_name}"
-echo Creating S3 bucket "${bucket_name}", S3 URI: "${s3_bucket_uri}".
-aws s3 mb "${s3_bucket_uri}"
-echo S3 bucket name: "${bucket_name}" >> output.txt
-echo S3 bucket URI: "${s3_bucket_uri}" >> output.txt
+rm output.txt
 
-# Upload bitstamp-properties.json.encrypted to S3 bucket
-echo Uploading bitstamp-properties.json.encrypted to S3 bucket "${bucket_name}"
-aws s3 cp bitstamp-properties.json.encrypted "${s3_bucket_uri}"
+npm install
+chmod 644 index.js
+chmod -R 644 node_modules
+zip -q -r "${bitcoin_a_day}" index.js node_modules 
 
-# Create IAM policy to access bitstamp-properties.json.encrypted
-iam_policy_name=bitcoin-a-day-"${random_id}"-bitstamp-properties-access
-sed -e s!@bucket_name@!"${bucket_name}"!g bitstamp-properties-access-policy-template.json > bitstamp-properties-access-policy.json
-echo Creating IAM policy "${iam_policy_name}" to access bitstamp-properties.json.encrypted file
-aws iam create-policy --policy-name "${iam_policy_name}" --policy-document file://bitstamp-properties-access-policy.json --output text
-echo IAM policy name: "${iam_policy_name}" >> output.txt
-iam_policy_arn_query='Policies[?PolicyName==`'"${iam_policy_name}"'`].Arn'
-iam_policy_arn=$(aws iam list-policies --query "${iam_policy_arn_query}" --output text)
-echo IAM policy ARN: "${iam_policy_arn}" >> output.txt
-
-# Create IAM role for the lambda function
-iam_role_name=bitcoin-a-day-"${random_id}"-lambda
+iam_role_name="${bitcoin_a_day}"-lambda
 echo Creating IAM role "${iam_role_name}" for the lambda function.
 aws iam create-role --role-name "${iam_role_name}" --assume-role-policy-document file://bitcoin-a-day-lambda-role-policy-document.json --query "Role.Arn" --output text
 iam_role_arn_query='Roles[?RoleName==`'"${iam_role_name}"'`].Arn'
@@ -41,18 +21,46 @@ iam_role_arn=$(aws iam list-roles --query "${iam_role_arn_query}" --output text)
 echo IAM role name for the lambda function: ${iam_role_name} >> output.txt
 echo IAM role ARN for the lambda function: ${iam_role_arn} >> output.txt
 
-echo Attaching IAM policy "${iam_policy_arn}" to lambda function role "${iam_role_name}"
-aws iam attach-role-policy --role-name "${iam_role_name}" --policy-arn "${iam_policy_arn}"
-
 echo Attaching IAM policy arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole to lambda function role "${iam_role_name}"
 aws iam attach-role-policy --role-name "${iam_role_name}" --policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole
+sleep 5s
 
 echo Creating encryption key policy.
+owner_id=$(aws ec2 describe-security-groups --group-names 'Default' --query 'SecurityGroups[0].OwnerId' --output text)
 sed -e "s!@owner_id@!${owner_id}!g; s!@iam_role_arn@!${iam_role_arn}!g" encryption-key-policy-template.json > encryption-key-policy.json
 echo Creating encryption key.
 encryption_key_id=$(aws kms create-key --policy fileb://encryption-key-policy.json --description "Bitcoin A Day" --query KeyMetadata.KeyId --output text)
 echo Encryption key id: ${encryption_key_id} >> output.txt
-encryption_key_alias=alias/bitcoin-a-day-"${random_id}"-encryption-key
+encryption_key_alias=alias/"${bitcoin_a_day}"-encryption-key
 echo Creating encryption key alias "${encryption_key_alias}".
 aws kms create-alias --alias-name "${encryption_key_alias}" --target-key-id "${encryption_key_id}"
 echo Encryption key alias: ${encryption_key_alias} >> output.txt
+encryption_key_arn=$(aws kms describe-key --key-id "${encryption_key_id}" --query KeyMetadata.Arn)
+echo Encryption key ARN: "${encryption_key_arn}" >> output.txt
+
+echo Encrypting Bitstamp customer id, key and secret.
+encrypted_bitstamp_customer_id=$(aws kms encrypt --key-id "${encryption_key_id}" --plaintext "${bitstamp_customer_id}" --query CiphertextBlob --output text)
+encrypted_bitstamp_key=$(aws kms encrypt --key-id "${encryption_key_id}" --plaintext "${bitstamp_key}" --query CiphertextBlob --output text)
+encrypted_bitstamp_secret=$(aws kms encrypt --key-id "${encryption_key_id}" --plaintext "${bitstamp_secret}" --query CiphertextBlob --output text)
+echo Encrypted Bitstamp customer id: "${encrypted_bitstamp_customer_id}" >> output.txt
+echo Encrypted Bitstamp key: "${encrypted_bitstamp_key}" >> output.txt
+echo Encrypted Bitstamp secret: "${encrypted_bitstamp_secret}" >> output.txt
+
+echo Creating environment properties for the lambda function.
+sed -e "s!@encrypted_bitstamp_customer_id@!${encrypted_bitstamp_customer_id}!g; s!@encrypted_bitstamp_key@!${encrypted_bitstamp_key}!g; s!@encrypted_bitstamp_secret@!${encrypted_bitstamp_secret}!g; s!@value@!${value}!g" bitcoin-a-day-env-template.json > bitcoin-a-day-env.json
+
+echo Creating lambda function.
+lambda_function_name="${bitcoin_a_day}"
+lambda_function_arn=$(aws lambda create-function --function-name "${lambda_function_name}" --runtime nodejs6.10 --role "$iam_role_arn" --handler index.handler --environment file://bitcoin-a-day-env.json --kms-key-arn "${encryption_key_arn}" --zip-file fileb://"${bitcoin_a_day}".zip --query FunctionArn --output text)
+echo Lambda function name: "${lambda_function_name}" >> output.txt
+echo Lambda function ARN: "${lambda_function_arn}" >> output.txt
+
+echo Creating CloudWatch rule to trigger the lambda function.
+trigger_rule_name="${lambda_function_name}"-rule
+trigger_rule_arn=$(aws events put-rule --name "${trigger_rule_name}" --schedule-expression "cron(0/1 * * * ? *)" --query RuleArn --output text)
+echo CloudWatch trigger rule name: "${trigger_rule_name}" >> output.txt
+echo CloudWatch trigger rule ARN: "${trigger_rule_arn}" >> output.txt
+trigger_rule_statement_id="${trigger_rule_name}"-event
+aws lambda add-permission --function-name "${lambda_function_name}" --statement-id "${trigger_rule_statement_id}" --action "lambda:InvokeFunction" --principal events.amazonaws.com --source-arn "${trigger_rule_arn}"
+sed -e "s!@lambda_function_arn@!${lambda_function_arn}!g" targets-template.json > targets.json
+aws events put-targets --rule "${trigger_rule_name}" --targets file://targets.json
